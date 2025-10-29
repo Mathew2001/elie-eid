5281e68fa5f4e4231e09b2b6c9f86406d6f644da
Mathew2001 <mathew2001abed@gmail.com>
2025-10-29 20:55:15 +0200

    da

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import { Routes, Route, HashRouter } from 'react-router-dom';
+import React from 'react';
+import { Routes, Route, HashRouter, Navigate } from 'react-router-dom';
 import Function1 from './components/Function1';
 import Function2 from './components/Function2';
 import Home from './components/Home';
 
-function App() {
+export default function App() {
   return (
     <HashRouter>
       <Routes>
+        <Route index element={<Home />} />               {/* renders at "#/" */}
         <Route path="/" element={<Home />} />
         <Route path="/function1" element={<Function1 />} />
         <Route path="/function2" element={<Function2 />} />
-        <Route path="*" element={<Home />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </HashRouter>
-  )
+  );
 }
-
-export default App;
